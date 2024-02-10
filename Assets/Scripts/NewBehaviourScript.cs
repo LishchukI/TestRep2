243aa7dde58a82620b80e0b3c5624de28d36c717
LishchukI <47892825+LishchukI@users.noreply.github.com>
2024-02-10 12:14:10 +0200

    Update NewBehaviourScript.cs

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("wet");
+        Debug.Log("wetr");
     }
 
     // Update is called once per frame
