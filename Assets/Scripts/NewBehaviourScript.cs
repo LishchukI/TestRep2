107e757d256513d0239dc0e1bb845ddc52ccfaa2
LishchukI <47892825+LishchukI@users.noreply.github.com>
2024-02-10 12:13:57 +0200

    back

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,15 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         Debug.Log("wet");
-=======
-        
->>>>>>> parent of 7c04199 (2)
-=======
-        
->>>>>>> parent of 7c04199 (2)
     }
 
     // Update is called once per frame
